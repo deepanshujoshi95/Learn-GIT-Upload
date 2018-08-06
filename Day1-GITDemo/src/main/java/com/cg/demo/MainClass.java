fd96bf0f6d68a65e4383889dd94dfcde5c1e0c3b
dejoshi <dejoshi@DIN73003013.corp.capgemini.com>
2018-08-06 10:28:54 +0530

    My First Commit

---
+++ Day1-GITDemo/src/main/java/com/cg/demo/MainClass.java
@@ -0,0 +1,15 @@
+package com.cg.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		great();
+	}
+	
+	public static void great()
+	{
+		System.out.println("Welcome");
+	}
+}
